60e14be4d2b27daa1fabfc3cede751d33f92f22c
Rodrigo Linares <rodolinares19@gmail.com>
2026-02-11 22:42:24 -0500

    feat: setup basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./product/product.component').then((m) => m.ProductComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./cart/cart.component').then((m) => m.CartComponent),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('avtr-frontend');
-}
+export class App {}
